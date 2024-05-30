c0029bfcdf4af5387a3a39f3f13391fa7d257267
Mahmudul-hasan-dev <contact.mahmudul.h@gmail.com>
2024-05-30 13:11:11 +0600

    make admin

--- index.js
+++ index.js
@@ -57,6 +57,10 @@ async function run() {
 
 
   //Users
+  app.get('/users',   async (req, res) => {
+    const result = await userCollection.find().toArray();
+    res.send(result);
+  });
     app.post('/users', async (req, res) => {
       const user = req.body;
      const query = { email: user.email }
@@ -67,6 +71,18 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.patch('/users/admin/:id',  async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
     //Class
     app.get('/classes', async(req, res) =>{
